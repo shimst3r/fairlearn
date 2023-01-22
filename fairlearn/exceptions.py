33a2901781de431ffc8256d8223de4769d287684
Nils Müller <shimst3r@gmail.com>
2023-01-22 22:04:04 +0100

    Implement FutureWarning for as_frame

--- fairlearn/exceptions.py
+++ fairlearn/exceptions.py
@@ -6,3 +6,10 @@ class DataFairnessWarning(UserWarning):
     """Custom warning meant to be raised when loading datasets with fairness issues."""
 
     pass
+
+
+class ImplicitUsageOfAsFrameWarning(FutureWarning):
+    """
+    Custom warning meant to be raised when loading datasets without explicitly
+    passing `as_frame`.
+    """
